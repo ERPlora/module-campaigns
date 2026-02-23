10e963330a32e7ef9955a4e20b81b99c6cb65116
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,31 +1,31 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'campaigns'
-    MODULE_NAME = _('Marketing Campaigns')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'megaphone-outline'
-    MODULE_DESCRIPTION = _('Plan, execute and track marketing campaigns')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'marketing'
+MODULE_ID = 'campaigns'
+MODULE_NAME = _('Marketing Campaigns')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'megaphone-outline'
+MODULE_DESCRIPTION = _('Plan, execute and track marketing campaigns')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'marketing'
 
-    MENU = {
-        'label': _('Marketing Campaigns'),
-        'icon': 'megaphone-outline',
-        'order': 52,
-    }
+MENU = {
+    'label': _('Marketing Campaigns'),
+    'icon': 'megaphone-outline',
+    'order': 52,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Campaigns'), 'icon': 'megaphone-outline', 'id': 'campaigns'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'campaigns.view_campaign',
+PERMISSIONS = [
+    'campaigns.view_campaign',
 'campaigns.add_campaign',
 'campaigns.change_campaign',
 'campaigns.delete_campaign',
 'campaigns.manage_settings',
-    ]
+]
